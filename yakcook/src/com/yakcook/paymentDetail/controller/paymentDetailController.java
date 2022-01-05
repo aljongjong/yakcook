d334be5be7715127215e1973777540d90369a6e1
wersfv <wersfv1105@naver.com>
2022-01-05 14:04:51 +0900

    결제 관리
    결제 관리 시작
    리뷰관리 중단

---
+++ yakcook/src/com/yakcook/paymentDetail/controller/paymentDetailController.java
@@ -0,0 +1,17 @@
+package com.yakcook.paymentDetail.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/paymentdetail")
+public class paymentDetailController extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/views/paymentDetail.jsp").forward(req, resp);
+	}
+}
